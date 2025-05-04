d5f8817cda40c68294bee3af15f9641ba09fdeae
Scott Densmore <scottdensmore@mac.com>
2025-05-03 20:57:05 -0700

    Update to move the scripts into the actual build phase

--- Utilities/update-oui.sh
+++ Utilities/update-oui.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-curl http://standards.ieee.org/develop/regauth/oui/oui.txt | grep "..-..-.." | grep hex | sed "s/(hex)//" | awk -F '\t' '{print $1,$3}' | awk -F '       ' '{print $1"\t"$3}' | sed -e 's/^[ \t]*//' > ../Resources/oui.txt 
+if [ -d ../Resources ]; then
+    curl http://standards.ieee.org/develop/regauth/oui/oui.txt | grep "..-..-.." | grep hex | sed "s/(hex)//" | awk -F '\t' '{print $1,$3}' | awk -F '       ' '{print $1"\t"$3}' | sed -e 's/^[ \t]*//' > ../Resources/oui.txt
+else
+    curl http://standards.ieee.org/develop/regauth/oui/oui.txt | grep "..-..-.." | grep hex | sed "s/(hex)//" | awk -F '\t' '{print $1,$3}' | awk -F '       ' '{print $1"\t"$3}' | sed -e 's/^[ \t]*//' > Resources/oui.txt
+fi
